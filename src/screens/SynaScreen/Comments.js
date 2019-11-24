a509f4e4d0e0c0cf4be9795264516ffe0f7db6b6
idanlevi1 <idanlevi2@gmail.com>
2019-11-24 23:59:19 +0200

    clean file

--- src/screens/SynaScreen/Comments.js
+++ src/screens/SynaScreen/Comments.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import {View, Platform, Text, TouchableOpacity, FlatList} from 'react-native';
-import {CommentListItem} from '../../components';
-import {styles} from './styles';
-import {en, he} from '../../constants';
+import React, { Component } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { CommentListItem } from '../../components';
+import { styles } from './styles';
+import { en, he } from '../../constants';
 
 export class Comments extends Component {
   constructor(props) {
@@ -25,7 +25,7 @@ export class Comments extends Component {
             {isEnglish ? en.detail.comments : he.detail.comments}
           </Text>
         </View>
-        <View style={{marginTop: 10}}>
+        <View style={{ marginTop: 10 }}>
           <FlatList
             data={this.props.item}
             renderItem={this.renderComments}
@@ -37,10 +37,10 @@ export class Comments extends Component {
     );
   }
 
-  renderComments = ({item, index}) => {
+  renderComments = ({ item, index }) => {
     return (
       <View style={styles.rowView}>
-        <CommentListItem onPress={() => {}} item={item} />
+        <CommentListItem onPress={() => { }} item={item} />
       </View>
     );
   };
